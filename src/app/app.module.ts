66156dc26dab5f5fb48e4c99dddaa971eaf5efa2
Topdevman <pkpavlo27@yahoo.com>
2017-02-20 00:07:40 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,45 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { PostsComponent } from './posts/posts.component';
+import { PostsService } from './posts.service';
+import { TasksComponent } from './tasks/tasks.component';
+import { TasksService } from './tasks.service';
+import { AuthenticationService } from '../services/authentication.service';
+
+// Define the routes
+const ROUTES = [
+  {
+    path: '',
+    redirectTo: 'posts',
+    pathMatch: 'full',
+  },
+  {
+    path: 'posts',
+    component: PostsComponent
+  },
+  {
+    path: 'tasks/all',
+    component: TasksComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsComponent,
+    TasksComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    RouterModule.forRoot(ROUTES)
   ],
-  providers: [],
+  providers: [PostsService, TasksService, AuthenticationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
